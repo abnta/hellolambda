8f790b1fc4056538c662a6daecb1aca109437273
Ieshan Tapan  Sharma <ieshantapansharma@Ieshans-MacBook-Air.local>
2024-07-27 01:11:08 +0530

    initial commit

--- lib/lambda-deploy-stack.ts
+++ lib/lambda-deploy-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+const lambda = cdk.aws_lambda;
 
 export class LambdaDeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaDeployQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new lambda.Function(this, 'HelloLambda', {
+      runtime: Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('../src'),
+      handler: 'index.handler',
+      functionName: 'helloLambda',
+    })
+    
   }
 }
